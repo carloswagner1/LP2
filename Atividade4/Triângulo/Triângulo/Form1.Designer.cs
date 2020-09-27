776fda604bbd11b6935ef231694d2308330f5a7f
Carlos Wagner <61124810+carloswagner1@users.noreply.github.com>
2020-09-27 17:00:48 -0300

    Add files via upload

---
+++ Atividade4/Triângulo/Triângulo/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Triângulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Lados = new System.Windows.Forms.Label();
+            this.lbl_C = new System.Windows.Forms.Label();
+            this.lbl_B = new System.Windows.Forms.Label();
+            this.lbl_A = new System.Windows.Forms.Label();
+            this.txt_A = new System.Windows.Forms.TextBox();
+            this.txt_B = new System.Windows.Forms.TextBox();
+            this.txt_C = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Lados
+            // 
+            this.lbl_Lados.AutoSize = true;
+            this.lbl_Lados.Font = new System.Drawing.Font("Bookman Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Lados.Location = new System.Drawing.Point(147, 30);
+            this.lbl_Lados.Name = "lbl_Lados";
+            this.lbl_Lados.Size = new System.Drawing.Size(329, 38);
+            this.lbl_Lados.TabIndex = 0;
+            this.lbl_Lados.Text = "Lados do triângulo";
+            // 
+            // lbl_C
+            // 
+            this.lbl_C.AutoSize = true;
+            this.lbl_C.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_C.Location = new System.Drawing.Point(173, 234);
+            this.lbl_C.Name = "lbl_C";
+            this.lbl_C.Size = new System.Drawing.Size(35, 32);
+            this.lbl_C.TabIndex = 1;
+            this.lbl_C.Text = "C";
+            // 
+            // lbl_B
+            // 
+            this.lbl_B.AutoSize = true;
+            this.lbl_B.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_B.Location = new System.Drawing.Point(173, 172);
+            this.lbl_B.Name = "lbl_B";
+            this.lbl_B.Size = new System.Drawing.Size(34, 32);
+            this.lbl_B.TabIndex = 2;
+            this.lbl_B.Text = "B";
+            // 
+            // lbl_A
+            // 
+            this.lbl_A.AutoSize = true;
+            this.lbl_A.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_A.Location = new System.Drawing.Point(173, 110);
+            this.lbl_A.Name = "lbl_A";
+            this.lbl_A.Size = new System.Drawing.Size(34, 32);
+            this.lbl_A.TabIndex = 3;
+            this.lbl_A.Text = "A";
+            // 
+            // txt_A
+            // 
+            this.txt_A.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_A.Location = new System.Drawing.Point(253, 106);
+            this.txt_A.Name = "txt_A";
+            this.txt_A.Size = new System.Drawing.Size(196, 36);
+            this.txt_A.TabIndex = 5;
+            this.txt_A.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_A_KeyPress);
+            // 
+            // txt_B
+            // 
+            this.txt_B.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_B.Location = new System.Drawing.Point(253, 168);
+            this.txt_B.Name = "txt_B";
+            this.txt_B.Size = new System.Drawing.Size(196, 36);
+            this.txt_B.TabIndex = 6;
+            this.txt_B.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_B_KeyPress);
+            // 
+            // txt_C
+            // 
+            this.txt_C.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_C.Location = new System.Drawing.Point(253, 230);
+            this.txt_C.Name = "txt_C";
+            this.txt_C.Size = new System.Drawing.Size(196, 36);
+            this.txt_C.TabIndex = 7;
+            this.txt_C.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_C_KeyPress);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.LightGray;
+            this.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVerificar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnVerificar.Location = new System.Drawing.Point(53, 306);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(151, 55);
+            this.btnVerificar.TabIndex = 8;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.LightGray;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(236, 306);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(151, 55);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.LightGray;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(419, 306);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(151, 55);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(622, 383);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txt_C);
+            this.Controls.Add(this.txt_B);
+            this.Controls.Add(this.txt_A);
+            this.Controls.Add(this.lbl_A);
+            this.Controls.Add(this.lbl_B);
+            this.Controls.Add(this.lbl_C);
+            this.Controls.Add(this.lbl_Lados);
+            this.Name = "Form1";
+            this.Text = "Triângulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Lados;
+        private System.Windows.Forms.Label lbl_C;
+        private System.Windows.Forms.Label lbl_B;
+        private System.Windows.Forms.Label lbl_A;
+        private System.Windows.Forms.TextBox txt_A;
+        private System.Windows.Forms.TextBox txt_B;
+        private System.Windows.Forms.TextBox txt_C;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade4/Triângulo/Triângulo/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triângulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txt_A.Clear();
+            txt_B.Clear();
+            txt_C.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            if (txt_A.Text == "" || txt_B.Text == "" || txt_C.Text == "")
+            {
+                MessageBox.Show("Todos os valores devem ser informados!!!", "Atenção");
+            }
+            else
+                if ((double.TryParse(txt_A.Text, out a)) &&
+                     (double.TryParse(txt_B.Text, out b)) &&
+                        (double.TryParse(txt_C.Text, out c)))
+            {
+                if ((Math.Abs(b-c) > a) || (a > b + c) || (Math.Abs(a-c) > b) || (b > a + c) || (Math.Abs(a-b) > c) || (c > a + b))
+                {
+                    MessageBox.Show("Os valores informados não formam um triângulo!!!");
+                }
+                else
+                {
+                    if (a != b && a != c && b != c)
+                    {
+                        MessageBox.Show("Triângulo Escaleno.", "Tipo de Triângulo");
+                    }
+                    else
+                        if (a == b && a == c)
+                    {
+                        MessageBox.Show("Triângulo Equilátero.", "Tipo de Triângulo");
+
+                    }
+                    else 
+                        if (a == b || a == c || b == c)
+                    {
+                        MessageBox.Show("Triângulo Isósceles.", "Tipo de Triângulo");
+                    }
+                }
+            }
+                
+        }
+
+        private void txt_A_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_B_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_C_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+    }
+}
