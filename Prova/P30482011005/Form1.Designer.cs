f3c17505742ec05a325ace778318b4cb959192be
Carlos Wagner <cwrsilva@gmail.com>
2020-11-17 10:48:53 -0300

    Avaliação LP2

---
+++ Prova/P30482011005/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace P30482011005
+{
+    partial class frmAvaliacao
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstbxResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(109, 202);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(208, 99);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstbxResultado
+            // 
+            this.lstbxResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbxResultado.FormattingEnabled = true;
+            this.lstbxResultado.ItemHeight = 20;
+            this.lstbxResultado.Location = new System.Drawing.Point(443, 49);
+            this.lstbxResultado.Name = "lstbxResultado";
+            this.lstbxResultado.Size = new System.Drawing.Size(391, 404);
+            this.lstbxResultado.TabIndex = 1;
+            // 
+            // frmAvaliacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(875, 503);
+            this.Controls.Add(this.lstbxResultado);
+            this.Controls.Add(this.btnExecutar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmAvaliacao";
+            this.Text = "Avaliação";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstbxResultado;
+    }
+}
+
---
+++ Prova/P30482011005/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P30482011005
+{
+    public partial class frmAvaliacao : Form
+    {
+        public frmAvaliacao()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[,] vendas = new double[5, 4];
+            string valor;
+            double totalPorMes = 0;
+            double totalGeral = 0;
+
+            for (int i = 0; i < vendas.GetLength(0); i++)
+            {
+                for (int j = 0; j < vendas.GetLength(1); j++)
+                {
+                    while (true)
+                    {
+                        valor = Interaction.InputBox("Mês: " + (i + 1) + "\nSemana: " + (j + 1), "Entrada de dados");
+
+                        if (double.TryParse(valor, out vendas[i, j]))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Valor Invalido!");
+                        }
+                    }
+                }
+            }
+            lstbxResultado.BeginUpdate();
+            for (int i = 0; i < vendas.GetLength(0); i++)
+            {
+                for (int j = 0; j < vendas.GetLength(1); j++)
+                {
+                    lstbxResultado.Items.Add("Total da Semana " + (j + 1) + " do Mês " + (i+1) + ": " + vendas[i,j].ToString("C2"));
+                    totalGeral += vendas[i, j];
+                    totalPorMes += vendas[i, j];
+                }               
+                lstbxResultado.Items.Add(">>>Total do mês " + (i + 1) + ": " + totalPorMes.ToString("C2"));
+                totalPorMes = 0;
+                lstbxResultado.Items.Add("--------------------");
+            }
+            lstbxResultado.Items.Add("--------------------");
+            lstbxResultado.Items.Add("Total Geral: " + totalGeral.ToString("C2"));
+            lstbxResultado.EndUpdate();
+        }
+    }
+}
