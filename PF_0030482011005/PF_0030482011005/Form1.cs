15ee83c2b815d6f0e13c77c3055b772e50d8666c
Carlos Wagner <cwrsilva@gmail.com>
2020-12-01 12:34:39 -0300

    Projeto Final

---
+++ PF_0030482011005/PF_0030482011005/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace PF_0030482011005
+{
+    public partial class frmPrincipal : Form
+    {
+
+        public static SqlConnection conexao;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // aqui a conexão vai depende da sua máquina da escola ou particular
+                conexao = new SqlConnection("Data Source=DESKTOP-2SPCNCN\\SQLEXPRESS;Initial Catalog=LP2;Integrated Security=True");
+                conexao.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Erro de banco de dados =/" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Outros Erros =/" + ex.Message);
+            }
+        }
+
+        private void cadastroCidadesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // testar se o form já está na memória
+            Form fc = Application.OpenForms["frmCidade"];
+
+
+
+            if (fc != null)
+                fc.Close();
+
+            frmCidade frmC = new frmCidade();
+            frmC.MdiParent = this;
+            frmC.WindowState = FormWindowState.Maximized;
+            frmC.Show();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSobre frmS = new frmSobre();
+            frmS.Show();
+        
+        }
+    }
+}
---
+++ PF_0030482011005/PF_0030482011005/frmSobre.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace PF_0030482011005
+{
+    partial class frmSobre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSobre));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblProjetoFinal = new System.Windows.Forms.Label();
+            this.lblDisciplina = new System.Windows.Forms.Label();
+            this.lblProfessora = new System.Windows.Forms.Label();
+            this.lblFatec = new System.Windows.Forms.Label();
+            this.gpbxAgradecimentos = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDesenvolvido = new System.Windows.Forms.Label();
+            this.lblAluno = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gpbxAgradecimentos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(704, 408);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(102, 36);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-88, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 506);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblProjetoFinal
+            // 
+            this.lblProjetoFinal.AutoSize = true;
+            this.lblProjetoFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProjetoFinal.Location = new System.Drawing.Point(465, 9);
+            this.lblProjetoFinal.Name = "lblProjetoFinal";
+            this.lblProjetoFinal.Size = new System.Drawing.Size(110, 20);
+            this.lblProjetoFinal.TabIndex = 2;
+            this.lblProjetoFinal.Text = "Projeto Final";
+            // 
+            // lblDisciplina
+            // 
+            this.lblDisciplina.AutoSize = true;
+            this.lblDisciplina.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisciplina.Location = new System.Drawing.Point(466, 37);
+            this.lblDisciplina.Name = "lblDisciplina";
+            this.lblDisciplina.Size = new System.Drawing.Size(254, 16);
+            this.lblDisciplina.TabIndex = 3;
+            this.lblDisciplina.Text = "Disciplina: Linguagem de Programação II";
+            // 
+            // lblProfessora
+            // 
+            this.lblProfessora.AutoSize = true;
+            this.lblProfessora.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProfessora.Location = new System.Drawing.Point(466, 53);
+            this.lblProfessora.Name = "lblProfessora";
+            this.lblProfessora.Size = new System.Drawing.Size(169, 16);
+            this.lblProfessora.TabIndex = 4;
+            this.lblProfessora.Text = "Professora Denilce Veloso";
+            // 
+            // lblFatec
+            // 
+            this.lblFatec.AutoSize = true;
+            this.lblFatec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFatec.Location = new System.Drawing.Point(466, 69);
+            this.lblFatec.Name = "lblFatec";
+            this.lblFatec.Size = new System.Drawing.Size(143, 16);
+            this.lblFatec.TabIndex = 5;
+            this.lblFatec.Text = "Fatec Sorocaba - 2020";
+            // 
+            // gpbxAgradecimentos
+            // 
+            this.gpbxAgradecimentos.Controls.Add(this.textBox1);
+            this.gpbxAgradecimentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbxAgradecimentos.Location = new System.Drawing.Point(446, 159);
+            this.gpbxAgradecimentos.Name = "gpbxAgradecimentos";
+            this.gpbxAgradecimentos.Size = new System.Drawing.Size(379, 243);
+            this.gpbxAgradecimentos.TabIndex = 6;
+            this.gpbxAgradecimentos.TabStop = false;
+            this.gpbxAgradecimentos.Text = "Agradecimentos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Moccasin;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 241);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // lblDesenvolvido
+            // 
+            this.lblDesenvolvido.AutoSize = true;
+            this.lblDesenvolvido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesenvolvido.Location = new System.Drawing.Point(466, 85);
+            this.lblDesenvolvido.Name = "lblDesenvolvido";
+            this.lblDesenvolvido.Size = new System.Drawing.Size(111, 16);
+            this.lblDesenvolvido.TabIndex = 7;
+            this.lblDesenvolvido.Text = "Desevolvido por:";
+            // 
+            // lblAluno
+            // 
+            this.lblAluno.AutoSize = true;
+            this.lblAluno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAluno.Location = new System.Drawing.Point(466, 101);
+            this.lblAluno.Name = "lblAluno";
+            this.lblAluno.Size = new System.Drawing.Size(216, 16);
+            this.lblAluno.TabIndex = 8;
+            this.lblAluno.Text = "Carlos Wagner Rodrigues da Silva";
+            // 
+            // frmSobre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(838, 466);
+            this.Controls.Add(this.lblAluno);
+            this.Controls.Add(this.lblDesenvolvido);
+            this.Controls.Add(this.gpbxAgradecimentos);
+            this.Controls.Add(this.lblFatec);
+            this.Controls.Add(this.lblProfessora);
+            this.Controls.Add(this.lblDisciplina);
+            this.Controls.Add(this.lblProjetoFinal);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "frmSobre";
+            this.Text = "frmSobre";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gpbxAgradecimentos.ResumeLayout(false);
+            this.gpbxAgradecimentos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblProjetoFinal;
+        private System.Windows.Forms.Label lblDisciplina;
+        private System.Windows.Forms.Label lblProfessora;
+        private System.Windows.Forms.Label lblFatec;
+        private System.Windows.Forms.GroupBox gpbxAgradecimentos;
+        private System.Windows.Forms.Label lblDesenvolvido;
+        private System.Windows.Forms.Label lblAluno;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
